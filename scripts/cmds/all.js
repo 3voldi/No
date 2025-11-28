0a9eb37a3ee814220d1aa108f90329a2e1c48935
3voldi <voldigoanos65@gmail.com>
2025-11-28 22:41:58 +0000

    Update all.js

--- scripts/cmds/all.js
+++ scripts/cmds/all.js
@@ -6,37 +6,40 @@ module.exports = {
 		countDown: 5,
 		role: 1,
 		description: {
-			vi: "Tag tất cả thành viên trong nhóm chat của bạn",
+			fr: "Tague tous les membres du groupe de discussion",
 			en: "Tag all members in your group chat"
 		},
 		category: "box chat",
 		guide: {
-			vi: "   {pn} [nội dung | để trống]",
+			fr: "   {pn} [texte à envoyer | laisser vide pour '@all']",
 			en: "   {pn} [content | empty]"
 		}
 	},
 
 	onStart: async function ({ message, event, args }) {
 		const { participantIDs } = event;
-		const lengthAllUser = participantIDs.length;
+		const totalMembres = participantIDs.length;
 		const mentions = [];
-		let body = args.join(" ") || "@all";
-		let bodyLength = body.length;
+		let texte = args.join(" ") || "@all";
+		let texteLength = texte.length;
 		let i = 0;
+
 		for (const uid of participantIDs) {
 			let fromIndex = 0;
-			if (bodyLength < lengthAllUser) {
-				body += body[bodyLength - 1];
-				bodyLength++;
+			if (texteLength < totalMembres) {
+				texte += texte[texteLength - 1];
+				texteLength++;
 			}
-			if (body.slice(0, i).lastIndexOf(body[i]) != -1)
+			if (texte.slice(0, i).lastIndexOf(texte[i]) != -1)
 				fromIndex = i;
 			mentions.push({
-				tag: body[i],
-				id: uid, fromIndex
+				tag: texte[i],
+				id: uid, 
+				fromIndex
 			});
 			i++;
 		}
-		message.reply({ body, mentions });
+
+		message.reply({ body: texte, mentions });
 	}
 };
